0750ee2eb5e982224161b4c79485b1d97485867e
HiPrabel <prabel397@gmail.com>
2025-06-29 20:53:17 +0530

    Todo app with contextApi & Redux

---
+++ 11reduxToolkitTodo/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
+
+function App() {
+
+  return (
+    <>
+      <div>Redux Toolkit</div>
+      <AddTodo />
+      <Todos />
+    </>
+  )
+}
+
+export default App
